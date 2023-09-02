29f12eb94648b500a17d8c76f34c55ae877e206b
mt123456888 <85531836+mt123456888@users.noreply.github.com>
2023-09-02 17:20:16 +0800

    Update 大师兄影视.js

--- js/大师兄影视.js
+++ js/大师兄影视.js
@@ -22,7 +22,7 @@ var rule = {
 		4:{cateId:'4'}
 	},
 	class_parse:'.navbar-items.swiper-wrapper li;a&&title;a&&href;/(\\d+).html',
-//	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
 
 	// searchUrl:'/search-**----------fypage---/',
 	searchUrl:'/index.php/ajax/suggest?mid=1&wd=**&limit=50',
